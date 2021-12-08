f18f920f0a31c3300a509b1f769f0db23322c38e
MarianaCRuiz <mchierandaruiz0gmail.com>
2021-12-08 16:55:38 -0300

    fix test i18n

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,19 @@
 class PurchasesController < ApplicationController
   def create
-    @client = current_client
-    @book = Book.find(params[:book_id])
-    @purchase = Purchase.generate(params)
+    setting_purchase(params)
     if @purchase
       BookClient.create!(book: @book, client: @client, final_price: @purchase[:final_price])
       redirect_to clients_books_path, notice: 'Compra efetuada com sucesso'
     else
       redirect_to books_path, notice: 'Sistema indisponível'
     end
+  end
+
+  private
+
+  def setting_purchase(params)
+    @client = current_client
+    @book = Book.find(params[:book_id])
+    @purchase = Purchase.generate(params)
   end
 end
--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'Managing books' do
   it '#index' do
-    book1 = create(:book)
-    book2 = create(:book)
+    book1 = create(:book, price: 10.0, discount: 1.2)
+    book2 = create(:book, price: 13.0, discount: 1.4)
 
     visit root_path
     click_on 'Livros'
@@ -12,11 +12,11 @@ describe 'Managing books' do
     expect(page).to have_content('Livros')
     expect(page).to have_content(book1.name)
     expect(page).to have_content(book1.author)
-    expect(page).to have_content(book1.price)
-    expect(page).to have_content(book1.discount)
+    expect(page).to have_content('R$ 10,00')
+    expect(page).to have_content('1,20%')
     expect(page).to have_content(book2.name)
     expect(page).to have_content(book2.author)
-    expect(page).to have_content(book2.price)
-    expect(page).to have_content(book2.discount)
+    expect(page).to have_content('R$ 13,00')
+    expect(page).to have_content('1,40%')
   end
 end
